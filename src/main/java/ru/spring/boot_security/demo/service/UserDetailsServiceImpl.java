de12badf18a26cce3bc06d0f318ced53740604b2
Igor Anikeev <anikeev@softlab.ru>
2024-02-09 22:32:30 +0400

    Рефракторинг

--- src/main/java/ru/spring/boot_security/demo/service/UserDetailsServiceImpl.java
+++ src/main/java/ru/spring/boot_security/demo/service/UserDetailsServiceImpl.java
@@ -5,9 +5,11 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ru.spring.boot_security.demo.dao.UserDao;
 
 @Service("userDetailsServiceImpl")
+@Transactional
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     private final UserDao userDao;
